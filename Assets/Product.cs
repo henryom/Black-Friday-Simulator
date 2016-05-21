3a2566a3137ce25c135b16950f71b094c4c2c815
henryom <Henry@Henrys-MacBook-Pro-47.local>
2016-05-21 13:59:24 -0700

    Working on code

---
+++ Assets/Product.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Product : GameObject {
+	int price;
+	int salePercentage;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/spawnItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnItem : MonoBehaviour {
+
+	public Product[] depElectronics;
+
+
+	// Use this for initialization
+	void addProduct (string department, Vector3 location) {
+		switch (department) {
+		case "electronics":
+			var product = depElectronics [Random.Range(0, depElectronics.Length - 1)];
+			product.price = 
+				product.salePercentage = Random.Range(15, 95);
+			Instantiate(product, location);
+			break;
+		}
+	
+	}
+}
